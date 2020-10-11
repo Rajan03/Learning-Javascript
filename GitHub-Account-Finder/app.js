3ab49727b4c04e33553f9aedb8a8f8b42849937f
Rajan Verma <65783031+Rajan03@users.noreply.github.com>
2020-10-11 23:05:47 +0530

    Update app.js

--- GitHub-Account-Finder/app.js
+++ GitHub-Account-Finder/app.js
@@ -1,30 +1,25 @@
-// Init Github
+//init
 const github = new Github();
-// Init UI
 const ui = new UI();
 
 // Search input
-const searchUser = document.getElementById("searchUser");
-
-// Search input event listener
-searchUser.addEventListener("keyup", (e) => {
-  // Get input text
+const searchUser = document.getElementById("searchUser").addEventListener("keyup", (e) => {
+  //input text
   const userText = e.target.value;
 
   if (userText !== "") {
-    // Make http call
     github.getUser(userText).then((data) => {
       if (data.profile.message === "Not Found") {
-        // Show alert
+        //alert
         ui.showAlert("User not found", "alert alert-danger");
       } else {
-        // Show profile
+        //profile
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
       }
     });
   } else {
-    // Clear profile
+    // Clear profile 
     ui.clearProfile();
   }
 });
