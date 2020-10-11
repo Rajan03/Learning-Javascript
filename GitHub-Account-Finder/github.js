c77d12d3a54bc35bfae4654b966dc71161d100ec
Rajan Verma <65783031+Rajan03@users.noreply.github.com>
2020-10-11 23:07:46 +0530

    Update github.js

--- GitHub-Account-Finder/github.js
+++ GitHub-Account-Finder/github.js
@@ -1,9 +1,8 @@
 class Github {
   constructor() {
-    this.client_id = "d9308aacf8b204d361fd";
-    this.client_secret = "84969aeef73956f4ec9e8716d1840532802bb81b";
+    this.client_id = "de060669162fcc342cb3";
+    this.client_secret = "ae529c4d8dd8ef6d645321a7d566d6224537687e";
     this.repos_count = 5;
-    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -12,7 +11,7 @@ class Github {
     );
 
     const repoResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
     const profile = await profileResponse.json();
